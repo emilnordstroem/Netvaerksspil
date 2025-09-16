2f1db75813ca90c11896f1c0c3a39be7361a771e
emilnordstroem <emilstoeve@outlook.com>
2025-09-16 10:04:01 +0200

    Server side

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
-
-
 import javafx.application.Application;
 
 public class App {
-
 	public static void main(String[] args) {
-		
-			Application.launch(GUI.class);	
-
+			Application.launch(GUI.class);
 	}
 }
---
+++ src/Server.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+    private static ServerSocket welcomeSocket;
+    private static HashMap<Socket, DataOutputStream> clientSockets;
+
+    public static void main(String[] args) {
+        try {
+            welcomeSocket = new ServerSocket(10_000);
+            clientSockets = new HashMap<>();
+
+            while (true) {
+                Socket connectionSocket = welcomeSocket.accept();
+                enableDataOutputFromClient(connectionSocket);
+                Thread readThread = new Thread(() -> readFromClient(connectionSocket));
+                readThread.start();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void readFromClient(Socket connectionSocket){
+        while (true) {
+            try {
+                BufferedReader inFromClient = new BufferedReader(
+                        new InputStreamReader(
+                                connectionSocket.getInputStream()
+                        )
+                );
+                String messageFromClient = inFromClient.readLine();
+                System.out.println("received from client: " + messageFromClient);
+                if (messageFromClient.toLowerCase().contains("disconnect")) {
+                    clientSockets.remove(connectionSocket);
+                    messageFromClient = "[" + connectionSocket.getInetAddress() + " has left the server]";
+                }
+                writeToClients(messageFromClient);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void writeToClients(String messageFromClient){
+        String messageToClients = messageFromClient.trim();
+        clientSockets.forEach((socket, outputStream) -> {
+            try {
+                System.out.println("message send by: " + socket.getInetAddress());
+                outputStream.writeBytes(messageToClients + '\n');
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static void enableDataOutputFromClient(Socket connectionSocket) {
+        try {
+            DataOutputStream outputStream = new DataOutputStream(
+                    connectionSocket.getOutputStream()
+            );
+            System.out.println("[client connected to Server from IP: " + connectionSocket.getInetAddress() + "]");
+            clientSockets.put(connectionSocket, outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
