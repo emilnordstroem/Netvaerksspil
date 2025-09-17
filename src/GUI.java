9aed9cc7305f9ce1030340f22bc9010be548df49
emilnordstroem <emilstoeve@outlook.com>
2025-09-17 12:53:47 +0200

    GUI method layout restructure

--- src/GUI.java
+++ src/GUI.java
@@ -73,7 +73,30 @@ public class GUI extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-        try {
+		// TCP connectionSetup
+		etablishTCPConnection();
+
+		// GUI grid and board setup
+		GridPane grid = gridPaneSetup();
+
+		Scene scene = new Scene(
+				grid,
+				scene_width,
+				scene_height
+		);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		// handle key presses
+		handleKeyPress(scene);
+
+		// Setting up standard players
+		settingUpStandardPlayers();
+
+	}
+
+	private void etablishTCPConnection () {
+		try {
 			clientSocket = new Socket(host, port);
 			outToServer = new DataOutputStream(
 					clientSocket.getOutputStream()
@@ -81,63 +104,78 @@ public class GUI extends Application {
 			inFromPlayer = new BufferedReader(
 					new InputStreamReader(System.in)
 			);
+		} catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-			GridPane grid = new GridPane();
-			grid.setHgap(10);
-			grid.setVgap(10);
-			grid.setPadding(new Insets(0, 10, 0, 10));
+	private GridPane gridPaneSetup () {
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 10, 0, 10));
 
-			Text mazeLabel = new Text("Maze:");
-			mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		Text mazeLabel = new Text("Maze:");
+		mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			Text scoreLabel = new Text("Score:");
-			scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+		Text scoreLabel = new Text("Score:");
+		scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			scoreList = new TextArea();
+		scoreList = new TextArea();
 
-			GridPane boardGrid = new GridPane();
+		// board setup
+		GridPane boardGrid = new GridPane();
 
-			image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+		image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
+		image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
 
-			hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
-			hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
-			hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
-			hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+		hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
+		hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
+		hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
+		hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
 
-			fields = new Label[20][20];
+		fields = new Label[20][20];
+		try {
 			for (int j=0; j<20; j++) {
 				for (int i=0; i<20; i++) {
 					switch (board[j].charAt(i)) {
-					case 'w':
-						fields[i][j] = new Label("", new ImageView(image_wall));
-						break;
-					case ' ':
-						fields[i][j] = new Label("", new ImageView(image_floor));
-						break;
-					default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
+						case 'w':
+							fields[i][j] = new Label("", new ImageView(image_wall));
+							break;
+						case ' ':
+							fields[i][j] = new Label("", new ImageView(image_floor));
+							break;
+						default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
 					}
 					boardGrid.add(fields[i][j], i, j);
 				}
 			}
-			scoreList.setEditable(false);
+		} catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        scoreList.setEditable(false);
 
-			grid.add(mazeLabel,  0, 0);
-			grid.add(scoreLabel, 1, 0);
-			grid.add(boardGrid,  0, 1);
-			grid.add(scoreList,  1, 1);
+		// Add elements
+		grid.add(mazeLabel,  0, 0);
+		grid.add(scoreLabel, 1, 0);
+		grid.add(boardGrid,  0, 1);
+		grid.add(scoreList,  1, 1);
 
-			Scene scene = new Scene(grid,scene_width,scene_height);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		scoreList.setText(getScoreList());
 
-			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-				switch (event.getCode()) {
-					// Concept:
-					// eventListener på key pressed (fortæller server move)
-					// bevæger sig kun ved server message (instruktion)
-					// formel for besked: player, move, x-move, y-move
-					// direction refere til billede - brug move.tolowercase()
+		return grid;
+	}
+
+	private void handleKeyPress(Scene scene){
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			switch (event.getCode()) {
+				// Concept:
+				// eventListener på key pressed (fortæller server move)
+				// bevæger sig kun ved server message (instruktion)
+				// formel for besked: player, move, x-move, y-move
+				// direction refere til billede - brug move.tolowercase()
 				case UP:
 					playerMoved(0,-1,"up");
 					break;
@@ -151,26 +189,25 @@ public class GUI extends Application {
 					playerMoved(+1,0,"right");
 					break;
 				default: break;
-				}
-			});
-			
-            // Setting up standard players
-			
-			me = new Player("Orville",9,4,"up");
-			players.add(me);
-			fields[9][4].setGraphic(new ImageView(hero_up));
+			}
+		});
+	}
 
-			Player harry = new Player("Harry",14,15,"up");
-			players.add(harry);
-			fields[14][15].setGraphic(new ImageView(hero_up));
+	private void settingUpStandardPlayers () {
+		me = new Player("Orville",9,4,"up");
+		players.add(me);
+		fields[9][4].setGraphic(new ImageView(hero_up));
 
-			scoreList.setText(getScoreList());
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		Player harry = new Player("Harry",14,15,"up");
+		players.add(harry);
+		fields[14][15].setGraphic(new ImageView(hero_up));
 	}
 
 	private void writeToServer () {
+
+	}
+
+	private void readFromServer(){
 
 	}
 
